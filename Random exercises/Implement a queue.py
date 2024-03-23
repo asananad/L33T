117acc9ac7fe6d5c6051c59e4f8c088d07ee00ef
asananad <162662223+asananad@users.noreply.github.com>
2024-03-23 23:38:12 +0900

    added new problems

---
+++ Random exercises/Implement a queue.py
@@ -0,0 +1,11 @@
+# Implement a queue
+
+queue = [1,2,3,4,5]
+
+print(queue.pop(0))
+
+print(queue)
+
+queue.append(6)
+
+print(queue)
---
+++ Random exercises/Implement a stack.py
@@ -0,0 +1,11 @@
+# Implement a stack
+
+stack = [1,2,3,4,5]
+
+print(stack.pop(0))
+
+print(stack)
+
+stack.insert(0, 1)
+
+print(stack)
